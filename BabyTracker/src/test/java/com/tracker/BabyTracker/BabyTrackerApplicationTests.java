32f7184baa58a0b0067105d7b097b838347e6cc6
haifa00 <hnajdawi@skillstorm.com>
2025-01-15 08:51:33 -0500

    init java project

---
+++ BabyTracker/src/test/java/com/tracker/BabyTracker/BabyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.BabyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
