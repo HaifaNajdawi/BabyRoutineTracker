9f2b62ccf319a6edfe58f1dd4de7580ac20d5c98
haifa00 <hnajdawi@skillstorm.com>
2025-01-22 15:05:24 -0500

    Create password field

--- BabyTracker/src/main/java/com/tracker/BabyTracker/controller/UserController.java
+++ BabyTracker/src/main/java/com/tracker/BabyTracker/controller/UserController.java
@@ -5,10 +5,9 @@ import com.tracker.BabyTracker.model.User;
 import com.tracker.BabyTracker.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -16,17 +15,21 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
-        this.userService=userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping(path = "/newUser")
-    public ResponseEntity<?> addUser(@RequestBody User user){
-        if(userService.findByEmail(user.getEmail()) == null){
-        return new ResponseEntity<>(userService.createUser(user.getName(),user.getEmail()), HttpStatus.CREATED);}
-        else {
-            return new ResponseEntity<>(new ExistRecord("Already Exist").getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        if (userService.findByEmail(user.getEmail()) == null) {
+            return new ResponseEntity<>(userService.createUser(user.getName(), user.getEmail(), user.getPassword()), HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(new ExistRecord("Already Exist").getMessage(), HttpStatus.BAD_REQUEST);
         }
 
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(userService.allUsers(),HttpStatus.OK);
     }
 }
--- Schemas/init.sql
+++ Schemas/init.sql
@@ -1,6 +1,8 @@
-create table users(
+create table if not exists users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
+Alter Table users
+Add Column password varchar(255) not null;
