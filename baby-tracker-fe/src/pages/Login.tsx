3bb0ccfa40d4a199380bb82e2861cd785fbcdf4a
haifa00 <hnajdawi@skillstorm.com>
2025-02-12 13:01:23 -0500

    login page updated

---
+++ baby-tracker-fe/src/pages/Login.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+
+export const Login: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Data Submitted ", formData);
+  };
+  return (
+    <div className="login-container">
+      <div className="login-left-side"> </div>
+
+      <div className="login-right-side">
+        <div className="login-box">
+          <h2>Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
